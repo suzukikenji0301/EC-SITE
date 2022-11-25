9cbcfcb71b8019248897ec9dd44c1d833ee7613e
pockytabetai <kumagai1mayu@gmail.com>
2022-11-25 12:04:14 +0900

    first commit.

---
+++ src/main/java/jp/co/example/ecommerce_a/Ec202210aApplication.java
@@ -0,0 +1,13 @@
+package jp.co.example.ecommerce_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202210aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202210aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/example/ecommerce_a/Ec202210aApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.example.ecommerce_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec202210aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
