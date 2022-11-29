0e7c13a07f76c3abee3b556e8141d73bb7901af8
pockytabetai <kumagai1mayu@gmail.com>
2022-11-29 10:38:56 +0900

    コンフリクトの解消

--- src/main/java/jp/co/example/ecommerce_a/controller/LogoutController.java
+++ src/main/java/jp/co/example/ecommerce_a/controller/LogoutController.java
@@ -1,11 +1,11 @@
 package jp.co.example.ecommerce_a.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.servlet.http.HttpSession;
 
 /**
  * ログアウト機能を制御するコントローラー.
--- src/main/java/jp/co/example/ecommerce_a/repository/ToppingRepository.java
+++ src/main/java/jp/co/example/ecommerce_a/repository/ToppingRepository.java
@@ -4,43 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
-<<<<<<< HEAD
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-=======
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
->>>>>>> develop
 import org.springframework.stereotype.Repository;
 
 import jp.co.example.ecommerce_a.domain.Topping;
 
 /**
-<<<<<<< HEAD
- * toppingsテーブルを操作するRepositoryクラス.
- * 
- * @author kumagaimayu
-=======
  * toppingテーブルを操作するリポジトリ.
  * 
  * @author moriharanariki
->>>>>>> develop
  *
  */
 @Repository
 public class ToppingRepository {
-<<<<<<< HEAD
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 
-=======
-	
 	/**
 	 * Toppingオブジェクトを生成するローマッパー.
-	 * 
 	 */
->>>>>>> develop
 	private static final RowMapper<Topping> TOPPING_ROW_MAPPER = (rs, i) -> {
 		Topping topping = new Topping();
 		topping.setId(rs.getInt("id"));
@@ -49,7 +34,6 @@ public class ToppingRepository {
 		topping.setPriceL(rs.getInt("price_l"));
 		return topping;
 	};
-<<<<<<< HEAD
 
 	/**
 	 * トッピング一覧を取得する.
@@ -60,21 +44,6 @@ public class ToppingRepository {
 		String sql = "SELECT id, name, price_m, price_l FROM toppings;";
 		SqlParameterSource param = new MapSqlParameterSource();
 		List<Topping> toppingList = template.query(sql, param, TOPPING_ROW_MAPPER);
-=======
-	
-	@Autowired
-	private NamedParameterJdbcTemplate template;
-	
-	
-	/**
-	 * 全件検索を行います。
-	 * 
-	 * @return
-	 */
-	public List<Topping> findAll() {
-		String sql = "SELECT id,name,price_m, price_l FROM toppings ORDER BY price_m;";
-		List<Topping> toppingList = template.query(sql, TOPPING_ROW_MAPPER);
->>>>>>> develop
 		return toppingList;
 	}
 }
