1c32a71fbcc33e504e468b6eab39166232348a52
pockytabetai <kumagai1mayu@gmail.com>
2022-11-29 21:39:13 +0900

    GlobalExceptionControllerと4xx,5xx.htmlの作成

---
+++ src/main/java/jp/co/example/ecommerce_a/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package jp.co.example.ecommerce_a;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * アプリケーション内でキャッチされなかった例外をここでキャッチし、エラーページに繊維させる.
+ * 
+ * @author kumagaimayu
+ *
+ */
+@Component
+public class GlobalExceptionHandler implements HandlerExceptionResolver {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+	@Override
+	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object obj,
+			Exception e) {
+		LOGGER.error("システムエラーが発生しました！", e);
+		return null;
+	}
+}
