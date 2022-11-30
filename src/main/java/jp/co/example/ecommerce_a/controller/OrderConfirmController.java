8547295e6c49d3f11f316a4b470c26e315ce74e7
moriharanariki <nariki@gmail.com>
2022-11-30 23:36:50 +0900

    バグ改修を行いました。

--- src/main/java/jp/co/example/ecommerce_a/controller/OrderConfirmController.java
+++ src/main/java/jp/co/example/ecommerce_a/controller/OrderConfirmController.java
@@ -3,9 +3,11 @@ package jp.co.example.ecommerce_a.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.example.ecommerce_a.domain.Order;
+import jp.co.example.ecommerce_a.form.OrderForm;
 import jp.co.example.ecommerce_a.service.OrderConfirmService;
 @Controller
 @RequestMapping("/orderConfirm")
@@ -21,7 +23,8 @@ public class OrderConfirmController {
 	 * @return　注文確認画面
 	 */
 	@RequestMapping("/")
-	public String orderConfirm(Integer orderId,Model model) {
+	public String orderConfirm(Integer orderId,Model model,OrderForm orderForm) {
+		
 		Order order = orderConfirmService.orderConfirm(15);
 		model.addAttribute("order",order);
 		return "order_confirm";
