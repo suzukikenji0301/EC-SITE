01d336cc6795910ec06e7aa7a2fc8b55d985b8a2
moriharanariki <nariki@gmail.com>
2022-12-01 21:46:55 +0900

    注文確認画面の表示がされるように実装を行いました。

--- src/main/java/jp/co/example/ecommerce_a/controller/OrderConfirmController.java
+++ src/main/java/jp/co/example/ecommerce_a/controller/OrderConfirmController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.example.ecommerce_a.domain.Order;
+import jp.co.example.ecommerce_a.form.OrderForm;
 import jp.co.example.ecommerce_a.service.OrderConfirmService;
 @Controller
 @RequestMapping("/orderConfirm")
@@ -21,7 +22,7 @@ public class OrderConfirmController {
 	 * @return　注文確認画面
 	 */
 	@RequestMapping("/orderConfirm")
-	public String orderConfirm(Integer orderId,Model model) {
+	public String orderConfirm(Integer orderId,Model model,OrderForm orderForm) {
 		System.out.println(orderId);
 		Order order = orderConfirmService.orderConfirm(orderId);
 		model.addAttribute("order",order);
