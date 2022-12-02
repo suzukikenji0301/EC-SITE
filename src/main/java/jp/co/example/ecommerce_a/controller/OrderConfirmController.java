bf0bd89a14237ea1787881abe0d9dda4dd03b270
HirabukiKoichiro <h.koichiro0402@gmail.com>
2022-12-02 09:44:20 +0900

    注文確認画面の合計金額・消費税の反映機能を追加しました。

--- src/main/java/jp/co/example/ecommerce_a/controller/OrderConfirmController.java
+++ src/main/java/jp/co/example/ecommerce_a/controller/OrderConfirmController.java
@@ -25,7 +25,12 @@ public class OrderConfirmController {
 	public String orderConfirm(Integer orderId,Model model,OrderForm orderForm) {
 		System.out.println(orderId);
 		Order order = orderConfirmService.orderConfirm(orderId);
+		double tax = order.getCalcTotalPrice();
+		tax = tax / 1.1;
+		tax = tax * 0.1;
 		model.addAttribute("order",order);
+		model.addAttribute("tax", tax);
+		model.addAttribute("totalPrice", order.getCalcTotalPrice());
 		return "order_confirm";
 	
 	}
